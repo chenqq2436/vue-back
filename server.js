e9fc357320ec34eb4efead09564721c54d5f8b42
chenqiang <1252321427@qq.com>
2021-01-05 12:52:25 +0800

    'init'

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+let users = [
+  { id: 1, name: "cehnqiang" },
+  { id: 2, name: "sunhanli" },
+  { id: 3, name: "chenfangfang" },
+];
+const server = http.createServer(function (req, res) {
+  // 解决跨域
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (req.url === "/api/users") {
+    res.end(JSON.stringify(users));
+  } else {
+    res.end("Not Found");
+  }
+});
+server.listen(3000, () => {
+  console.log("后端接口服务器已经启动在3000端口");
+});
